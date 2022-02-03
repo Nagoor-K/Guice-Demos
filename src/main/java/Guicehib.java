a3bdb94b2b16a2f2e22c2551fe55d76bbe5504c5
Nagoor-K <p.nagoor@axelor.com>
2022-02-03 17:51:07 +0530

    hibernate jpa pravtice with guice

---
+++ src/main/java/Guicehib.java
@@ -0,0 +1,11 @@
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+public class Guicehib {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+	  Injector injector=Guice.createInjector(new )
+
+  }
+}
--- src/main/java/MyModule.java
+++
@@ -1,8 +0,0 @@
-import com.google.inject.AbstractModule;
-
-public class MyModule extends AbstractModule{
-	@Override
-	public void configure() {
-		bind(DrawShape.class).to(DrawSquare.class);
-	}
-}
